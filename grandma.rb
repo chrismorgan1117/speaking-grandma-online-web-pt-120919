09e12d85086955ff83becd7151b4449a83317ff5
Christopher Morgan <cwm111794@gmail.com>
2019-12-10 00:53:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
     elsif phrase == "WHAT DID YOU EAT TODAY?" || "WHAT?"
     puts "NO, NOT SINCE 1938!"
   else 
-    puts
+    puts 
 end 
 
 # Whatever you say to grandma, she should respond with
