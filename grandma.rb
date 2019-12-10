ba5cee21da2bb9438f61b651c14ade3f3c946462
Christopher Morgan <cwm111794@gmail.com>
2019-12-10 00:37:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUUMPKIN!"
-    elsif phrase == ""
+    elsif phrase == "Hi Nana, how are you?"
 end 
 
 # Whatever you say to grandma, she should respond with
