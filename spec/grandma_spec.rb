ffec4c5444ccec40fd2e76553822886c32942410
Christopher Morgan <cwm111794@gmail.com>
2019-12-10 00:55:36 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,22 +1,22 @@
 describe '#speak_to_grandma' do
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
-    expect(speak_to_grandma(phrase)('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
   end
 
   it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
-    expect(speak_to_grandma(phrase)('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
+    expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
   end
 
   it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
-    expect(speak_to_grandma(phrase)('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
+    expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
   end
 
   it 'responds with NO, NOT SINCE 1938! when she can hear you' do
-    expect(speak_to_grandma(phrase)('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
+    expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
   end
 
   it 'responds with NO, NOT SINCE 1938! for a second time' do
-    expect(speak_to_grandma(phrase)('WHAT?')).to eq "NO, NOT SINCE 1938!"
+    expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
   end
 
 end
