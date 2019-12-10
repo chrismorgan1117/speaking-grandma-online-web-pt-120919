4c5237602dbe34da74dfda4c3797079d0577473b
Christopher Morgan <cwm111794@gmail.com>
2019-12-10 00:53:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    puts 
     elsif phrase == "WHAT DID YOU EAT TODAY?" || "WHAT?"
     puts "NO, NOT SINCE 1938!"
   else 
